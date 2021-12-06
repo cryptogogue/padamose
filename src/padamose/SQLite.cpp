51306b3123f97284528e6d0bfdbf773d4e0d252d
Patrick Meehan <patrickmeehan.git@fallguyconsulting.com>
2021-12-06 10:50:24 -0800

    added sql exec logging

--- src/padamose/SQLite.cpp
+++ src/padamose/SQLite.cpp
@@ -259,6 +259,9 @@ SQLiteResult SQLite::exec ( string sql, SQLPrepareCallbackFunc onPrepare ) {
 //----------------------------------------------------------------//
 SQLiteResult SQLite::exec ( string sql, SQLPrepareCallbackFunc onPrepare, SQLRowCallbackFunc onRow ) {
     
+    LGN_LOG_SCOPE ( PDM_FILTER_SQLITE, INFO, __PRETTY_FUNCTION__ );
+    LGN_LOG ( PDM_FILTER_SQLITE, INFO, "%s", sql.c_str ());
+    
     sqlite3_stmt* stmt;
     
     SQLiteResult result = this->prepare ( sql, &stmt, onPrepare );
